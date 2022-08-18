33ed0aa45f19e15d1be511a1b34e90cc0550bcf6
tainamorais <tainafms@gmail.com>
2022-08-18 13:37:01 -0300

    Resolução da prática nº 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-pratica/ex-03.js
@@ -0,0 +1,11 @@
+const n1 = 9;
+const n2 = 7;
+const n3 = 3;
+
+if (n1 > n2 && n1 > n3) {
+  console.log(`O maior número é o ${n1}`);
+} else if (n2 > n1 && n2 > n3) {
+  console.log(`O maior número é o ${n2}`);
+} else {
+  console.log(`O maior número é o ${n3}`);
+}
