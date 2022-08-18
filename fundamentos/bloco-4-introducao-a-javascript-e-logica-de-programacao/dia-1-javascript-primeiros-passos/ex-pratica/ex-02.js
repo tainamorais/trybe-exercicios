7d0b619c7a5df800fd87021f0541455f1405a7f3
tainamorais <tainafms@gmail.com>
2022-08-18 13:16:13 -0300

    Resolução da prática nº 2.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-pratica/ex-02.js
@@ -0,0 +1,11 @@
+const n1 = 9;
+const n2 = 11;
+message = '';
+
+if (n1 > n2) {
+  console.log(n1);
+} else if (n2 > n1) {
+  console.log(n2);
+} else {
+  console.log('Os dois números são iguais.')
+}
