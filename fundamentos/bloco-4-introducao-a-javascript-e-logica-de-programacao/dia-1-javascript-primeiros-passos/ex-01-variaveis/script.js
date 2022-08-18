7d981b98967c0776278cb8818ac23d2aa8d26c8b
tainamorais <tainafms@gmail.com>
2022-08-18 11:08:20 -0300

    Resolução do exercício sobre variáveis

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-01-variaveis/script.js
@@ -0,0 +1,15 @@
+const myName = 'Tainá';
+const birthCity = 'Rio de Janeiro';
+let birthYear = 1983;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = 'Petrópolis';
+
+// console.log(birthCity); = constantes não podem ser alteradas
