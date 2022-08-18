082de6bf8f8915f53572aac448e627eae28eb94a
tainamorais <tainafms@gmail.com>
2022-08-18 13:09:44 -0300

    Resolução da prática nº 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-pratica/ex-01.js
@@ -0,0 +1,13 @@
+const a = 9;
+const b = 3;
+const adicao = a + b;
+const subtracao = a - b;
+const multiplicacao = a * b;
+const divisao = a / b;
+const modulo = a % b;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
