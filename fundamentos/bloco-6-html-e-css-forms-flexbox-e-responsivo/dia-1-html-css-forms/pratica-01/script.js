8584470c1f1add1702e418028ad44b765f86f626
tainamorais <tainafms@gmail.com>
2022-09-05 13:04:14 -0300

    Ajustes nos exercícios do final de bloco.

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/pratica-01/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/pratica-01/script.js
@@ -1,44 +1,60 @@
-//VER MELHOR OS EXERCÍCIOS DESTE BLOCO
-
 //Interrompa o comportamento padrão do botão submit utilizando o método preventDefault(). 
-const submitBtn = document.querySelector('#submitBtn');
-submitBtn.addEventListener('click', function(event) {
+function handleSubmit(event) {
   event.preventDefault();
-})
-
-/*
-Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: 'Dados Inválidos'. Caso contrário, a mensagem 'Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.' deverá aparecer na tela.
-*/
-const inputName = document.querySelector('#input-nome');
-const inputEmail = document.querySelector('#input-email');
-submitBtn.addEventListener('click', function() {
-  if (inputName.value.length < 10 || inputName.value.length > 40 || inputEmail.value.length < 10 || inputEmail.value.length > 50) {
-    alert('Dados inválidos')
+  const validation = textInputValidation();
+  if (validation === false) {
+    alert('Dados inválidos');
   } else {
-    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
   }
-})
+}
 
-
-//Função que limpa formulário
+//Função do botão apagar
 function clearFields() {
-  const formElements = document.querySelectorAll('input');
+  const inputElements = document.querySelectorAll('input');
   const textArea = document.querySelector('textarea');
-  for (let index = 0; index < formElements.length; index += 1) {
-    formElements[index].value = '';
-    formElements[index].checked = false;
+  for (let index = 0; index < inputElements.length; index += 1) {
+    const userInput = formElements[index];
+    userInput.value = '';
+    userInput.checked = false;
   }
   textArea.value = '';
 }
 
-const resetBtn = document.querySelector('#resetBtn');
-resetBtn.addEventListener('click', clearFields);
-
-
-//A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
-const checkboxImg = document.querySelector('#checkbox-imagens');
+//Faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
+//No HTML, o botão submit deve estar marcado como disable.
 function enableSubmit() {
+  const submitBtn = document.querySelector('#submitBtn');
+  const checkboxImg = document.querySelector('#checkbox-imagens');
   submitBtn.disabled = !checkboxImg.checked;
 }
-checkboxImg.addEventListener('change', enableSubmit);
 
+/*
+Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: 'Dados Inválidos'. Caso contrário, a mensagem 'Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.' deverá aparecer na tela.
+*/
+function textInputValidation() {
+  const email = document.querySelector('#input-email').value.length;
+  const invalidEmail = email < 10 || email > 50;
+
+  const name = document.querySelector('#input-nome').value.length;
+  const invalidName = name < 10 || name > 40;
+
+  const answer = document.querySelector('#resposta-concurso').value.length;
+  const invalidAnswer = answer> 500;
+
+  if (invalidEmail || invalidName || invalidAnswer) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+window.onload = function () {
+  const submitBtn = document.querySelector('#submitBtn');
+  submitBtn.addEventListener('click', handleSubmit);
+  const resetBtn = document.querySelector('#resetBtn');
+  resetBtn.addEventListener('click', clearFields);
+  const checkboxImg = document.querySelector('#checkbox-imagens');
+  checkboxImg.addEventListener('change', enableSubmit);
+};
