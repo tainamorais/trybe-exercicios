33e81c6b215e00fc3efeb3f7e1d074afeba71775
tainamorais <tainafms@gmail.com>
2022-12-05 07:50:09 -0300

    Ajustes dia 2

--- frontend/secao-07-gerenciamento-de-estado-com-redux/day-02/pratic-03-reference/src/index.js
+++ frontend/secao-07-gerenciamento-de-estado-com-redux/day-02/pratic-03-reference/src/index.js
@@ -17,3 +17,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/*
+https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/live-lectures/17907612-170d-4ac4-8332-db126bd1ef35/recording/4526722d-dd1b-4b6d-9dc2-cf3c49c8e246
+*/
